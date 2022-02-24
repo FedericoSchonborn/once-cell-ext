d65b61bea68202a5eaabc187e5e75ef6b38b1566
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-02-24 02:56:32 -0300

    Add support for std::lazy::OnceCell
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use once_cell::{sync, unsync};
+#![cfg_attr(feature = "std", feature(once_cell))]
 
 trait OnceCellExt<T> {
     fn set_or<E>(&mut self, value: T, error: E) -> Result<(), E>;
@@ -7,28 +7,28 @@ trait OnceCellExt<T> {
         F: FnOnce() -> E;
 }
 
-impl<T> OnceCellExt<T> for sync::OnceCell<T> {
-    fn set_or<E>(&mut self, value: T, error: E) -> Result<(), E> {
-        self.set(value).map_err(|_| error)
-    }
+macro_rules! once_cell_ext_impl {
+    ($($ty:ty),+) => {
+        $(impl<T> $crate::OnceCellExt<T> for $ty {
+            fn set_or<E>(&mut self, value: T, error: E) -> Result<(), E> {
+                self.set(value).map_err(|_| error)
+            }
 
-    fn set_or_else<E, F>(&mut self, value: T, error: F) -> Result<(), E>
-    where
-        F: FnOnce() -> E,
-    {
-        self.set(value).map_err(|_| error())
-    }
+            fn set_or_else<E, F>(&mut self, value: T, error: F) -> Result<(), E>
+            where
+                F: FnOnce() -> E,
+            {
+                self.set(value).map_err(|_| error())
+            }
+        })*
+    };
 }
 
-impl<T> OnceCellExt<T> for unsync::OnceCell<T> {
-    fn set_or<E>(&mut self, value: T, error: E) -> Result<(), E> {
-        self.set(value).map_err(|_| error)
-    }
+#[cfg(feature = "std")]
+once_cell_ext_impl! {
+    std::lazy::OnceCell<T>
+}
 
-    fn set_or_else<E, F>(&mut self, value: T, error: F) -> Result<(), E>
-    where
-        F: FnOnce() -> E,
-    {
-        self.set(value).map_err(|_| error())
-    }
+once_cell_ext_impl! {
+    once_cell::sync::OnceCell<T>, once_cell::unsync::OnceCell<T>
 }
